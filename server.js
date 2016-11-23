6bb198153c070e9d3310926beecfbf4d87ac5541
Fabio Norata <fabio.norata@digitaslbi.com>
2016-11-23 23:11:56 +0100

    config

--- server.js
+++ server.js
@@ -293,9 +293,6 @@ app.get('/brown', function (req, res) {
   res.render('index', getContext("brown"));
 });
 
-app.listen(port, function () {
-  console.log('Example app listening on port '+port);
-});
 app.listen(port, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
