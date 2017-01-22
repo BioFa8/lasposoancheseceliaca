8b47849936a00605825889261a27925d8601855f
Fabio Norata <fabio.norata@digitaslbi.com>
2017-01-22 19:25:35 +0100

    Immagini

--- data/dati.js
+++ data/dati.js
@@ -98,41 +98,28 @@ module.exports.guests = guests;
 var events = function(theme_color) {
   return [
     {
-      date: "03.12.2013",
-      image: global.images_path+"/"+theme_color+"/port7.jpg",
+      image: global.images_path+"/luna-colosseo.jpg",
+      youtube: "https://www.youtube.com/embed/O6D9YA2ZHjE?rel=0",
       title:"Colosseo",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+      desc: "Il Colosseo è il più grande anfiteatro del mondo, situato nel centro della città di Roma. In grado di contenere un numero di spettatori stimato tra 50.000 e 75.000 unità, è il più importante anfiteatro romano, nonché il più imponente monumento della Roma antica che sia giunto fino a noi, conosciuto in tutto il mondo come simbolo della città di Roma e uno dei simboli d'Italia."
     },
     {
-      date: "03.01.2014",
-      image: global.images_path+"/"+theme_color+"/port8.jpg",
-      youtube: "http://www.youtube.com/embed/hEowkXOP_Wg?rel=0",
+      image: global.images_path+"/fori-imperiali.jpg",
+      youtube: "https://www.youtube.com/embed/ZFyBG5874SY?rel=0",
       title:"Fori Imperiali",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+      desc: "I Fori Imperiali costituiscono una serie di piazze monumentali edificate nel corso di un secolo e mezzo (tra il 46 a.C. e il 113 d.C.) nel cuore della città di Roma da parte degli imperatori."
     },
     {
-      date: "12.05.2014",
-      image: global.images_path+"/"+theme_color+"/port9.jpg",
-      title:"Altare della Patria",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+      image: global.images_path+"/vittoriano-frecce-tricolori.jpg",
+      youtube: "https://www.youtube.com/embed/2G7TdtkJPos?rel=0",
+      title: "Il Vittoriano",
+      desc: "Il Monumento nazionale a Vittorio Emanuele II, meglio conosciuto con il nome di Vittoriano o Altare della Patria, è un monumento nazionale situato a Roma, sul Campidoglio, opera dell'architetto Giuseppe Sacconi. È uno dei simboli patri italiani."
     },
     {
-      date: "24.08.2014",
-      image: global.images_path+"/"+theme_color+"/port10.jpg",
-      title:"The Proposal",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
-    },
-    {
-      date: "10.01.2015",
-      image: global.images_path+"/"+theme_color+"/port11.jpg",
-      title:"Here comes the baby",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
-    },
-    {
-      date: "03.02.2015",
-      image: global.images_path+"/"+theme_color+"/port12.jpg",
-      title:"Bridesmaids",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+      image: global.images_path+"/san-pietro.jpg",
+      youtube: "https://www.youtube.com/embed/T9bUQWhoIxA?rel=0",
+      title:"Piazza San Pietro",
+      desc: "Piazza San Pietro è la piazza antistante la basilica di San Pietro. Posta a margine del centro storico di Roma, la piazza fa parte della Città del Vaticano ed è delimitata dal confine con lo Stato italiano; attraverso il rione Borgo che giace a est, i principali accessi sono da via di Porta Angelica o da via della Conciliazione."
     },
   ];
 };
